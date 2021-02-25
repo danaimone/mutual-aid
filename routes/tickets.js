51cc66cdc28201a1bb1d101b07bb6b85252cc915
dawc <dawc@wwu.edu>
2021-02-25 13:40:05 -0800

    Unify colors across sass and add tickets routing
    Also added a filewatcher in WebStorm to auto compile sass into css. I'm also pushing the css along with the sass

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mutual Aid' });
-});
-
 router.get('/tickets', function(req, res, next) {
   res.render('tickets', { title: 'Tickets', layout: false} );
 });
-
 
 module.exports = router;
