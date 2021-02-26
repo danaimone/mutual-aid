14e803aeac30c8046c759fb2eea4d6e2e4ff5575
dawc <dawc@wwu.edu>
2021-02-26 14:15:18 -0800

    Make consistent headers and footers site-wide
    Haven't added content to header or footer yet, but now everything looks the same

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,6 @@ router.get('/faq', function(req, res, next) {
   res.render('faq', { title: 'Tickets', layout: 'faq'} );
 });
 
-/* GET FAQ page. */
-router.get('/faq', function(req, res, next) {
-  res.render('faq', { title: 'faq', layout: false} );
-});
-
 /* GET Map page. */
 router.get('/map', function(req, res, next) {
   res.render('map', { title: 'Map - Mutual Aid', layout: false} );
