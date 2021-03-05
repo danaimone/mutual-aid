00817d4e15b3f2790faefd10c050d150c5988f42
Hannah Buzard <46615766+hannahbuzard1@users.noreply.github.com>
2021-03-04 22:34:33 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var authenticateController=require( __dirname + "/" + "authenticate-controller");
+var registerController=require( __dirname + "/" + "register-controller");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,5 +17,19 @@ router.get('/map', function(req, res, next) {
   res.render('map', { title: 'Map - Mutual Aid', layout: false} );
 });
 
+router.get('/registration', function (req, res) {
+   res.sendFile( __dirname + "/" + "registration.html" );
+})
+
+router.get('/login', function (req, res) {
+   res.sendFile( __dirname + "/" + "login.html" );
+})
+
+/* route to handle login and registration */
+router.post('/api/register',registerController.register);
+router.post('/api/authenticate',authenticateController.authenticate);
+
+router.post('/register-controller.js', registerController.register);
+router.post('/authenticate-controller.js', authenticateController.authenticate);
 
 module.exports = router;
