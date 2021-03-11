e56790078b295673910ff351dab3683cf7045fe6
Hannah Buzard <46615766+hannahbuzard1@users.noreply.github.com>
2021-03-11 01:58:56 -0800

    Add files via upload

---
+++ public/controllers/authenticate-controller.js
@@ -0,0 +1,57 @@
+let connection = require('./database');
+let Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+
+
+module.exports.authenticate=function(req,res){
+    let username=req.body.user;
+    let password=req.body.password;
+    var errorString = "";
+    if(username == "" || password == "") {
+      req.flash('error', "No Username and Password provided." );
+      req.flash('errorMsg', "");
+      res.redirect('/');
+        return;
+    }
+
+    let sql = 'SELECT password FROM Users WHERE username = @user;'
+    var request = new Request(sql, (err, rowCount) => {
+        if (err) {
+          req.flash('error', "Username does not exist." );
+          req.flash('errorMsg', "");
+          res.redirect('/');
+          return;
+        } else {
+            if (rowCount < 1) {
+              req.flash('error', "Username does not exist." );
+              req.flash('errorMsg', "");
+              res.redirect('/');
+            }
+        }
+    });
+
+    request.addParameter('user', TYPES.VarChar, username);
+    request.on('row', function(columns) {
+        console.log(columns);
+        if (columns.length < 1) {
+          req.flash('error', "Username does not exist." );
+          req.flash('errorMsg', "");
+          res.redirect('/');
+        }
+        else {
+            if (password === columns[0].value) {
+              req.flash('error', "" );
+              req.flash('errorMsg', "");
+              res.redirect('/');
+                return;
+            } else {
+              req.flash('error', "Password is incorrect." );
+              req.flash('errorMsg', "");
+              res.redirect('/');
+                return;
+            }
+        }
+    });
+
+    connection.execSql(request);
+}
---
+++ public/controllers/database.js
@@ -0,0 +1,31 @@
+const { Connection, Request } = require("tedious");
+
+// Create connection to database
+const config = {
+    authentication: {
+        options: {
+            userName: "dawc",
+            password: "ChrisDanHannahRobbie!" // TODO: Use environment variables for this!
+        },
+        type: "default"
+    },
+    server: "mutual-aid.database.windows.net",
+    options: {
+        database: "mutual-aid-website",
+        encrypt: true
+    }
+};
+const connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on("connect", err => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log("Database connected.");
+    }
+});
+
+connection.connect();
+
+module.exports = connection;
