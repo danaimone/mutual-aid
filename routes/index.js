f15a7bd22fbf15d8c167f130b65e1b7570cee346
Hannah Buzard <46615766+hannahbuzard1@users.noreply.github.com>
2021-03-11 01:57:01 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var authenticateController=require( __dirname + "/" + "authenticate-controller");
-var registerController=require( __dirname + "/" + "register-controller");
-
+var authenticateController=require("../public/controllers/authenticate-controller");
+var registerController=require("../public/controllers/register-controller");
+var ticketsController=require("../public/controllers/tickets-controller");
+var contactsController=require("../public/controllers/send-email");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mutual Aid' , layout: false});
+  res.render('index', { title: 'Mutual Aid' , layout: false, error: req.flash('error'), errorMsg: req.flash('errorMsg'), contact: req.flash('contacts') });
 });
 
 router.get('/faq', function(req, res, next) {
@@ -19,17 +20,22 @@ router.get('/map', function(req, res, next) {
 
 router.get('/registration', function (req, res) {
    res.sendFile( __dirname + "/" + "registration.html" );
-})
+});
 
 router.get('/login', function (req, res) {
    res.sendFile( __dirname + "/" + "login.html" );
-})
+});
 
 /* route to handle login and registration */
+/*
 router.post('/api/register',registerController.register);
 router.post('/api/authenticate',authenticateController.authenticate);
+router.post('/api/createTicket',ticketsController.createTicket);
+
+ */
 
 router.post('/register-controller.js', registerController.register);
 router.post('/authenticate-controller.js', authenticateController.authenticate);
+router.post('/send-email', contactsController.sendmail);
 
 module.exports = router;
