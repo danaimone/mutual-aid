8c71ab00774c59584a382bbc37957c309f4f50ce
Dan Aimone <danaimone@gmail.com>
2021-03-16 16:12:50 -0700

    finished navbar added logo added favicon

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,37 @@
 var express = require('express');
 var router = express.Router();
-var authenticateController=require("../public/controllers/authenticate-controller");
-var registerController=require("../public/controllers/register-controller");
-var commentController=require("../public/controllers/comment-controller");
-var contactsController=require("../public/controllers/send-email");
-let ticketsController=require("../public/controllers/tickets-controller");
+var authenticateController = require("../public/controllers/authenticate-controller");
+var registerController = require("../public/controllers/register-controller");
+var commentController = require("../public/controllers/comment-controller");
+var contactsController = require("../public/controllers/send-email");
+let ticketsController = require("../public/controllers/tickets-controller");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mutual Aid' , layout: false, error: req.flash('error'), errorMsg: req.flash('errorMsg'), contact: req.flash('contacts') });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Mutual Aid',
+        layout: false,
+        error: req.flash('error'),
+        errorMsg: req.flash('errorMsg'),
+        contact: req.flash('contacts')
+    });
 });
 
-router.get('/faq', function(req, res, next) {
-  res.render('faq', { title: 'Tickets', layout: 'faq'} );
+router.get('/faq', function (req, res, next) {
+    res.render('faq', {title: 'Tickets', layout: 'faq'});
 });
 
 /* GET Map page. */
-router.get('/map', function(req, res, next) {
-  res.render('map', { title: 'Map - Mutual Aid', layout: false} );
+router.get('/map', function (req, res, next) {
+    res.render('map', {title: 'Map - Mutual Aid', layout: false});
 });
 
 router.get('/registration', function (req, res) {
-   res.sendFile( __dirname + "/" + "registration.html" );
+    res.sendFile(__dirname + "/" + "registration.html");
 });
 
 router.get('/login', function (req, res) {
-   res.sendFile( __dirname + "/" + "login.html" );
+    res.sendFile(__dirname + "/" + "login.html");
 });
 
 
